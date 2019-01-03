d49c37763f81127a5697bdbf9506b0434772c9ef
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2019-01-03 13:22:16 +0100

    [TASK] Added constants

---
+++ source/vcl/GsVclConsts.pas
@@ -0,0 +1,38 @@
+{
+  This file is part of Gilbertsoft Delphi Library (GSDL).
+
+  Copyright (C) 2017-2018 Simon Gilli
+    Gilbertsoft | https://delphi.gilbertsoft.org
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+}
+unit GsVclConsts;
+
+interface
+
+resourcestring
+  { GSDynControlEngine.pas }
+  SDynControlEngineErrorNoFocusControl = 'TGsDynControlEngine.CreateLabledEdit: AEditControl must be assigned';
+
+  { GSObjectEditor.pas }
+  SObjectEditorDialogButtonOk = '&OK';
+  SObjectEditorDialogButtonCancel = '&Abbrechen';
+  SObjectEditorDialogCaptionEditProperties = 'Eigenschaften bearbeiten';
+  SObjectEditorDialogCaptionEditPropertiesOf = 'Eigenschaften von %s bearbeiten';
+  SObjectEditorErrorNoDataSupport = 'Keine Datenunterstützung für Property %s von %s!';
+  SObjectEditorEditObject = '%s bearbeiten';
+
+implementation
+
+end.
